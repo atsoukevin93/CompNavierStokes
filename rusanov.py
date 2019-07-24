1a3d2c1de5b4733d04072b43175792de3c370be4
atsoukevin93 <atsoukevin@gmail.com>
2019-07-24 23:34:53 +0200

    Méthode de Rusanov 1D

--- rusanov.py
+++ rusanov.py
@@ -1,10 +1,12 @@
 from fipy import *
 import numpy as np
 from matplotlib import pyplot as plt
-
+from fractions import Fraction
+# plt.ion()
+# plt.switch_backend('qt5agg')
 #Maillage 1D
 L = 1.0
-nx = 100
+nx = 20000
 dx = L / nx
 
 mesh = PeriodicGrid1D(dx, nx)
@@ -87,7 +89,7 @@ extCellFaces = mesh.cellFaceIDs[:,eexT]
 
 # NS barotrope
 c = 1.
-gamma = 1.4
+gamma = 2.
 
 
 
@@ -96,32 +98,49 @@ U1 = CellVariable(name='$u_1$', mesh=mesh, value=1., hasOld=True)
 # U1.setValue(1, where=x >= 0.5)
 # U1.setValue((np.sqrt(2))**(1./gamma), where=x < 0.5)
 
-U1.setValue(1, where=x >= 0.7)
-U1.setValue(1, where=x < 0.5)
-U1.setValue((np.sqrt(2))**(1./gamma), where=(x > 0.5) & (x < 0.7))
+# U1.setValue(1, where=x >= 0.7)
+# U1.setValue(1, where=x < 0.5)
+# U1.setValue((np.sqrt(2))**(1./gamma), where=(x > 0.5) & (x < 0.7))
 
-# U1.setValue(np.exp(-(x-0.5)**2))
+# U1.setValue(np.exp(-((x-0.5)**2)/(2 * 0.005)))
+
+U1.setValue(np.exp(-((x-0.25)**2)/(2 * 0.005)) + np.exp(-((x-0.75)**2)/(2 * 0.005)))
 
 U2 = CellVariable(name='$u_2$', mesh=mesh, value=0., hasOld=True)
 
 Flux1 = CellVariable(name="\mathcal{F_1}", mesh=mesh, value=0.)
 Flux2 = CellVariable(name="\mathcal{F_2}", mesh=mesh, value=0.)
 
-dt = 0.00001
+dt1 = 0.0001
 duration = 10
-Nt = int(duration / dt) + 1
+Nt = int(duration / dt1) + 1
+dt = dt1
 
+
+# Redefinition of numpy Power function
+# def power_modified(x, y):
+#     if np.mod(y, 2) == 0:
+#         return (x)**(y)
+#     return np.sign(x)*(np.abs(x))**(y)
 
 # Flux
 def Fe(x):
-    return np.array([x[1],((x[1]**2)/x[0])+c*x[0]**gamma])
+    return np.array([x[1], ((x[1]**2)/x[0])+c*(x[0])**gamma])
 
+
+def centered_mean(x,y):
+    return (x+y)/2.
+
+
+def harmonic_mean(x,y):
+    return 2*(x*y)/(x + y)
 # Loop in time
 
 
 # Draw figures
 # fig = plt.figure();
 # fig.canvas.draw();
+
 sp, axes = plt.subplots(1, 2)
 
 Rho = Matplotlib1DViewer(vars=U1, axes=axes[0],
@@ -132,31 +151,38 @@ Rho_u_Rho = Matplotlib1DViewer(vars=U2/U1, axes=axes[1], datamin=0.000,
 
 viewers = MultiViewer(viewers=(Rho, Rho_u_Rho))
 
-for n in range(Nt):
-    # The construction of the Fluxes
+tps = 0.
+while tps <= duration:
+
+    if U1.value.any() < 0.:
+        break
+
+    # The construction of Rusanov Fluxes
     U = np.array([U1, U2])
     Flux = np.array([Flux1, Flux2])
     lambdas = np.zeros(nVol)
 
     # --------------------------- The interior Faces ------------------
-    lambda1 = ((U[1]/U[0])- numerix.sqrt(c*gamma*U[0]**(gamma-1)))
-    lambda2 = ((U[1]/U[0]) + numerix.sqrt(c*gamma*U[0]**(gamma-1)))
+    lambda1 = 2*((U2/U1) - numerix.sqrt(c*gamma*(U1)**(gamma-1)))
+    lambda2 = 2*((U2/U1) + numerix.sqrt(c*gamma*(U1)**(gamma-1)))
 
     lambdas = numerix.maximum(numerix.maximum(lambda1[FacesCells[0]], lambda1[FacesCells[1]]),
                               numerix.maximum(lambda2[FacesCells[0]], lambda2[FacesCells[1]]))
 
-    Flux_plus = (Fe(U[:, FacesCells[0]]) + Fe(U[:, FacesCells[1]]))/2. - lambdas * (U[:, FacesCells[1]] - U[:, FacesCells[0]])/2.
+    # print(lambdas)
+
+    Flux_plus = (centered_mean(Fe(U[:, FacesCells[0]]), Fe(U[:, FacesCells[1]])) - lambdas * (U[:, FacesCells[1]] - U[:, FacesCells[0]])/2.)
 
     # shift ids to the left for the fluxes at i-0.5
-    Flux_moins = (Fe(U[:, FacesCells[0]-1]) + Fe(U[:, FacesCells[1]-1]))/2. - lambdas * (U[:, FacesCells[1]-1] - U[:, FacesCells[0]-1])/2.
+    Flux_moins = (centered_mean(Fe(U[:, FacesCells[0]-1]), Fe(U[:, FacesCells[1]-1]))) - lambdas * (U[:, FacesCells[1]-1] - U[:, FacesCells[0]-1])/2.
 
-    temp_Flux = Flux_plus - Flux_moins
+    # temp_Flux = Flux_plus - Flux_moins
 
     Flux[:, :] = Flux_plus[:, CellFaces[1]] - Flux_moins[:, CellFaces[0]]
 
-    # dt = dx/np.max(lambdas)
 
-    # print(dt)
+
+    print('time step: ', dt)
 
     Unew = U - (dt/dx)*Flux
 
@@ -165,6 +191,12 @@ for n in range(Nt):
 
     U1.updateOld()
     U2.updateOld()
+
+    dt = np.min([dt1, 0.5 * dx / (2. * np.max(lambdas.value))])
+
+    tps = tps + dt
+    # if np.isnan(dt):
+    #     break
 
     viewers.plot()
 
