04daec0a68e4c9ec90585fe24efabf33e3fc5807
baltaki <bilal.altaki.math@gmail.com>
2019-08-08 22:41:54 +0200

    maillage décalé simple

--- Explicit_staggered_scheme.py
+++ Explicit_staggered_scheme.py
@@ -53,8 +53,8 @@ Rho.setValue((np.sqrt(2.))**(1./gamma), where=(x > 0.5) & (x < 0.7))
 
 
 #
-#rho=Rho.value
-#U1=U.value
+rho=Rho.value
+U=U.value
 
 U_fig = CellVariable(name='$U$', mesh=mesh, value=0., hasOld=True)
 
@@ -71,27 +71,28 @@ def Explicit_Staggered(rho, U, dt, dx):
     F = (U + shiftg(U)) / 2.
     G= (U+shiftd(U))/ 2.
     #equation de consevation de la masse
-    diag0m = 1 -(dt/dx) * (pplus(U[0: N-1]) - pminus(U[0: N-1]))
-    diag0= -(dt/dx) * (pminus(U[1:N-1]))
-    diag0p=dt/dx * (pplus(U[0: N-1]))
+    diag0 = np.ones(N)  -(dt/dx) * (pplus(U[0: N]) - pminus(U[0: N]))
+    diag0m= (dt/dx) * (pminus(U[0:N]))
+    diag0p= -(dt/dx) * (pplus(U[1: N+1]))
     A = sp.lil_matrix(sp.spdiags([diag0m, diag0, diag0p], [-1, 0, 1], N, N))
     A[0, N-1] = dt/dx * (pplus(U[0]))
     A[N-1, 0] = -dt/dx *(pminus(U[N]))
     Rho_new= A.dot(Rho.value)
 
     # eq de conservation du moment, on l'écrit sous la forme AU=B
-    diag0 = (1/(2*dt) * (shiftd(Rho_new)+ Rho_new) + 1/(dx*dx)*(mu_rho(Rho_new)+shiftd(mu_rho(Rho_new))))
-    diag0p = np.concatenate([[0], (-1/(dx*dx)*mu_rho(Rho_new))[0:N]])
-    diag0m = np.concatenate([1/(dx*dx) * mu_rho(Rho_new)[0:N], [0]])
-    M = sp.lil_matrix(sp.spdiags([diag0m, diag0, diag0p], [-1, 0, 1], N + 1, N + 1))
+    Q= shiftd(Rho_new) + Rho_new + 1 / (dx * dx) * (mu_rho(Rho_new) + shiftd(mu_rho(Rho_new)))
+    diag0 = 1/(2*dt)*np.concatenate([Q,[Q[0]]])
+    diag0p = np.concatenate([[0], (-1/(dx*dx)*mu_rho(Rho_new))])
+    diag0m = np.concatenate([1/(dx*dx) * mu_rho(Rho_new)[0:N],[0]])
+    M = sp.lil_matrix(sp.spdiags([diag0m, diag0, diag0p], [-1, 0, 1], N +1 , N +1))
     M[0, N] = -mu_rho(Rho_new)[0] / (dx*dx)
     M[N, 0] = mu_rho(Rho_new)[N - 1] /(dx*dx)
 
-    B = 1/dt * (np.concatenate(shiftd(rho) + rho, (shiftd(rho)+ rho)[0]) * U
-      -1/dx * np.concatenate([barotrope_rhotoP(Rho_new, c, gamma) -shiftd(barotrope_rhotoP(Rho_new, c, gamma)),
-                            [barotrope_rhotoP(Rho_new, c, gamma) -shiftd(barotrope_rhotoP(Rho_new, c, gamma))[0]]])
-      -1/dx * np.concatenate([rho, [rho[0]]]) * (U * pplus(F) + shiftg(U) * pminus(F))
-      +1/dx * np.concatenate([shiftd(rho), [shiftd(rho)[0]]]) * (shiftd(U) * pplus(G) + U* pminus(G)))
+    R= barotrope_rhotoP(Rho_new, c, gamma) -shiftd(barotrope_rhotoP(Rho_new, c, gamma))
+    B = 1/dt * (np.concatenate([shiftd(rho) + rho, [(shiftd(rho)+ rho)[0]]]) * U)
+       - 1/dx * np.concatenate([R, [R[0]]])
+      - 1/dx * np.concatenate([rho, [rho[0]]]) * (U * pplus(F) + shiftg(U) * pminus(F))
+      + 1/dx * np.concatenate([shiftd(rho), [shiftd(rho)[0]]]) * (shiftd(U) * pplus(G) + U * pminus(G))
     U_new = splin.spsolve(M,B)
     return Rho_new, U_new
 
