44ed4b4465f2b233dd5ae76ad32a8d520a05fdfe
atsoukevin93 <atsoukevin@gmail.com>
2019-08-13 10:17:09 +0200

    Code pour effectuer les cas tests.

--- checkTestResults.py
+++ checkTestResults.py
@@ -24,52 +24,52 @@ def l1_error(U, dx, ax):
 def linf_error(U, ax):
     return np.max(np.abs(U), axis=ax)
 
-
-dirpath = "data/"
-filename_hkl = "hkl_test_euler"
+# ------------------------- Code for Saving figures and generating gifs ----------------------------------------
+# dirpath = "data/"
+# filename_hkl = "hkl_test_new"
 
 # files = os.listdir(dirpath)
 # files.sort()
 #
 # numb_to_test = 1
-
-data_hkl = np.load(dirpath+filename_hkl)
-
-Rhos_hkl = data_hkl['Rho']
-Us_hkl = data_hkl['U']
+#
+# data_hkl = np.load(dirpath+filename_hkl)
+#
+# Rhos_hkl = data_hkl['Rho']
+# Us_hkl = data_hkl['U']
 
 # --------------------- Time and Space steps ---------------------------------
 
-dts = data_hkl['dt']
-dxs = data_hkl['dx']
-t = data_hkl['t']
-Nt = t.shape[0]
-Nvol = Rhos_hkl.shape[1]
-dx = float(dxs[0])
+# dts = data_hkl['dt']
+# dxs = data_hkl['dx']
+# t = data_hkl['t']
+# Nt = t.shape[0]
+# Nvol = Rhos_hkl.shape[1]
+# dx = float(dxs[0])
 # dt = float(dxs[0])
 
 # -------------------- data for the rusanov method ----------------------------
-filename_rusanov = "rusanov_splitting_test_euler"
-
-data_rusanov = np.load(dirpath+filename_rusanov)
-
-Rhos_rusanov = data_rusanov['Rho']
-Us_rusanov = data_rusanov['U']
+# filename_rusanov = "rusanov_splitting_test_new"
+#
+# data_rusanov = np.load(dirpath+filename_rusanov)
+#
+# Rhos_rusanov = data_rusanov['Rho']
+# Us_rusanov = data_rusanov['U']
 
 # Données du maillage
 # Centres des mailles
-mesh = PeriodicGrid1D(dx, Nvol)
-x, = mesh.cellCenters
-x_faces = mesh.faceCenters
+# mesh = PeriodicGrid1D(dx, Nvol)
+# x, = mesh.cellCenters
+# x_faces = mesh.faceCenters
 
 # les inconnus
-U_hkl = FaceVariable(name='$u$', mesh=mesh, value=0.)
-# Rho0 = CellVariable(name='$\\rho$', mesh=mesh, value=0., hasOld=True)
-Rho_hkl = CellVariable(name='$\\rho_{hkl}$', mesh=mesh, value=0., hasOld=True)
-U_fig_hkl = CellVariable(name='$U_{hkl}$', mesh=mesh, value=0., hasOld=True)
-
-Rho_rusanov = CellVariable(name='$\\rho_{rus}$', mesh=mesh, value=0., hasOld=True)
-U_rusanov = CellVariable(name='$U_{rus}$', mesh=mesh, value=0., hasOld=True)
+# U_hkl = FaceVariable(name='$u$', mesh=mesh, value=0.)
+# # Rho0 = CellVariable(name='$\\rho$', mesh=mesh, value=0., hasOld=True)
+# Rho_hkl = CellVariable(name='$\\rho_{hkl}$', mesh=mesh, value=0., hasOld=True)
+# U_fig_hkl = CellVariable(name='$U_{hkl}$', mesh=mesh, value=0., hasOld=True)
+#
+# Rho_rusanov = CellVariable(name='$\\rho_{rus}$', mesh=mesh, value=0., hasOld=True)
+# U_rusanov = CellVariable(name='$U_{rus}$', mesh=mesh, value=0., hasOld=True)
 
 
 # sp, spa = plt.subplots(1, 2)
@@ -89,41 +89,132 @@ U_rusanov = CellVariable(name='$U_{rus}$', mesh=mesh, value=0., hasOld=True)
 #     plt.pause(2)  # pause avec duree en secondes
 #     t = t + dts[i]
 
-sp1, axes = plt.subplots(1,2)
+# sp1, axes = plt.subplots(1,2)
+#
+# Rho_fig = Matplotlib1DViewer(vars=(Rho_hkl, Rho_rusanov), axes=axes[0], interpolation='spline16', datamax=1.5, figaspect='auto')
+#
+# u_fig = Matplotlib1DViewer(vars=(U_fig_hkl, U_rusanov), axes=axes[1], interpolation='spline16', figaspect='auto')
+#
+# viewers = MultiViewer(viewers=(Rho_fig, u_fig))
+#
+# test_dir_path = "figures/hkl_vs_rusanov_tests/hkl_vs_rusanov_test_new/"
+#
+# if not os.path.exists(test_dir_path):
+#     os.makedirs(test_dir_path)
+#
+# plt.rcParams['font.weight'] = 'bold'
+# plt.rcParams.update({'font.size': 15})
+#
+# for i in range(Nt):
+#     Rho_hkl.setValue(Rhos_hkl[i])
+#     U_hkl.setValue(Us_hkl[i])
+#     U_fig_hkl.setValue((U_hkl + shiftg(U_hkl))[0:Nvol] / 2.)
+#
+#     Rho_rusanov.setValue(Rhos_rusanov[i])
+#     U_rusanov.setValue(Us_rusanov[i])
+#
+#     viewers.plot()
+#     plt.title("HKL vs Rusanov. tps={0}".format(str(round(t[i], 2))))
+#
+#     if i % 50 == 0:
+#         plt.savefig(test_dir_path+'hkl_vs_rusanov_test_new_tps{0}.png'.format(i))
+#     # plt.close()
 
-Rho_fig = Matplotlib1DViewer(vars=(Rho_hkl, Rho_rusanov), axes=axes[0], interpolation='spline16', datamax=1.5, figaspect='auto')
 
-u_fig = Matplotlib1DViewer(vars=(U_fig_hkl, U_rusanov), axes=axes[1], interpolation='spline16', figaspect='auto')
+# ------------------------------------- Error between Hkl and Rusanov Method -------------------------------------
+dirpath = "data/"
+Num_method = ['rusanov_splitting', 'hkl']
+files = os.listdir(dirpath)
+Nfiles = len(files)
+file_suffix = '_NS_test_'
+hkl_files = []
+rusanov_files = []
 
-viewers = MultiViewer(viewers=(Rho_fig, u_fig))
+L = 1
 
-test_dir_path = "figures/hkl_vs_rusanov_tests/hkl_vs_rusanov_Euler_test/"
+def get_nsteps(file_tab):
+    N = len(file_tab)
+    file_tab = np.array(file_tab)
+    temp = np.empty([], dtype=np.int)
+    temp = np.delete(temp, 0)
 
-if not os.path.exists(test_dir_path):
-    os.makedirs(test_dir_path)
+    for i in range(N):
+        num = int(file_tab[i].split('_')[-1])
+        temp = np.append(temp, num)
+    temp.sort()
+    return temp
+
+
+def sort_files(file_tab):
+    N = len(file_tab)
+    file_tab = np.array(file_tab)
+    temp = np.empty([], dtype=np.int)
+    temp = np.delete(temp, 0)
+
+    for i in range(N):
+        num = int(file_tab[i].split('_')[-1])
+        temp = np.append(temp, num)
+    temp.sort()
+
+    temp1 = np.empty([], dtype=np.str)
+    temp1 = np.delete(temp1, 0)
+    for i in range(N):
+        for j in range(N):
+            if int(file_tab[j].split('_')[-1]) == temp[i]:
+                temp1 = np.append(temp1, file_tab[j])
+
+    return temp1
+
+
+for i in range(Nfiles):
+    if files[i].startswith('rusanov'):
+        rusanov_files.append(files[i])
+    if files[i].startswith('hkl'):
+        hkl_files.append(files[i])
+
+rusanov_files = sort_files(rusanov_files)
+hkl_files = sort_files(hkl_files)
+Ntest = len(rusanov_files)
+
+rho_L2_norms = np.array([])
+u_L2_norms = np.array([])
+
+for i in range(Ntest):
+
+    data_rusanov = np.load(dirpath + Num_method[0]+file_suffix + rusanov_files[i].split('_')[-1])
+    dts = data_rusanov['dt']
+    dxs = data_rusanov['dx']
+    t = data_rusanov['t']
+    Nt = t.shape[0]
+    Nvol = Rhos_rusanov.shape[1]
+    dx = float(dxs[0])
+    Rhos_rusanov = data_rusanov['Rho']
+    Us_rusanov = data_rusanov['U']
+
+    data_hkl = np.load(dirpath + Num_method[1]+file_suffix + hkl_files[i].split('_')[-1])
+    Rhos_hkl = data_hkl['Rho']
+    Us_hkl = data_hkl['U']
+    Us_hkl_new = np.zeros(Us_hkl.shape)
+    rho_L2_norms = np.append(rho_L2_norms, l2_error(l2_error(Rhos_hkl-Rhos_rusanov, dx, 1), dts, 0))
+
+    for i in range(Nt):
+        Us_hkl_new[i] = (Us_hkl[i] + shiftg(Us_hkl[i]))[0:Nvol]/2.
+    u_L2_norms = np.append(u_L2_norms, l2_error(l2_error(Us_hkl_new-Us_rusanov, dx, 1), dts, 0))
+
+hs = L/get_nsteps(rusanov_files)
+hs.sort()
 
 plt.rcParams['font.weight'] = 'bold'
 plt.rcParams.update({'font.size': 15})
-
-for i in range(Nt):
-    Rho_hkl.setValue(Rhos_hkl[i])
-    U_hkl.setValue(Us_hkl[i])
-    U_fig_hkl.setValue((U_hkl + shiftg(U_hkl))[0:Nvol] / 2.)
-
-    Rho_rusanov.setValue(Rhos_rusanov[i])
-    U_rusanov.setValue(Us_rusanov[i])
-
-    viewers.plot()
-    plt.title("HKL vs Rusanov. tps={0}".format(str(round(t[i], 2))))
-
-    if i % 50 == 0:
-        plt.savefig(test_dir_path+'hkl_vs_rusanov_euler_test_tps{0}.png'.format(i))
-    # plt.close()
-
+plt.title('$L^2_t L^2_x$ error Hkl vs rusanov')
+plt.xlabel('$\Delta_x$')
+plt.ylabel('$\Vert \\rho_{hkl} - \\rho_{rus} \Vert_{L^2_t L^2_x}$')
+plt.plot(hs, rho_L2_norms[np.arange(Ntest - 1, -1, -1)], linewidth=3)
+plt.plot(hs, u_L2_norms[np.arange(Ntest - 1, -1, -1)], linewidth=3)
 # --------------------------------------L2 error---------------------------------------
-error_l2 = l2_error(Rhos_hkl-Rhos_rusanov[1:Nt+1], dx, 1)
-error_l1 = l1_error(Rhos_hkl-Rhos_rusanov[1:Nt+1], dx, 1)
-error_linf = linf_error(Rhos_hkl-Rhos_rusanov[1:Nt+1], 1)
+# error_l2 = l2_error(Rhos_hkl-Rhos_rusanov[1:Nt+1], dx, 1)
+# error_l1 = l1_error(Rhos_hkl-Rhos_rusanov[1:Nt+1], dx, 1)
+# error_linf = linf_error(Rhos_hkl-Rhos_rusanov[1:Nt+1], 1)
 
 #
 # plt.plot(t, error_l1)
